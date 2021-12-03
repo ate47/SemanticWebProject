de5b7b691fc492acaa5678fc8111c1196d9ea370
ATE47 <wilate@yahoo.fr>
2021-12-03 16:39:41 +0100

    add meteociel scraper

--- Scrapper/src/main/java/fr/atesab/sw/project/scraper/ScraperException.java
+++ Scrapper/src/main/java/fr/atesab/sw/project/scraper/ScraperException.java
@@ -2,18 +2,19 @@ package fr.atesab.sw.project.scraper;
 
 public class ScraperException extends Exception {
     public ScraperException(Scraper s) {
-        this("Can't scrap with " + s.getName());
+        super("Can't scrap with " + s.getName());
     }
 
     public ScraperException(Scraper s, Throwable e) {
-        this("Can't scrap with " + s.getName(), e);
+        super("Can't scrap with " + s.getName(), e);
     }
 
-    public ScraperException(String msg, Throwable e) {
-        super(msg, e);
+    public ScraperException(Scraper s, String msg, Throwable e) {
+        super("Can't scrap with " + s.getName() + ": " + msg, e);
     }
 
-    public ScraperException(String msg) {
-        super(msg);
+    public ScraperException(Scraper s, String msg) {
+        super("Can't scrap with " + s.getName() + ": " + msg);
     }
+
 }
--- Scrapper/src/main/java/fr/atesab/sw/project/scraper/meteo/MeteoScraper.java
+++ Scrapper/src/main/java/fr/atesab/sw/project/scraper/meteo/MeteoScraper.java
@@ -1,8 +1,7 @@
 package fr.atesab.sw.project.scraper.meteo;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.util.Iterator;
 
 import org.apache.jena.rdf.model.Model;
 import org.jsoup.Jsoup;
@@ -25,12 +24,37 @@ public class MeteoScraper implements Scraper {
         return "meteo_html";
     }
 
+    private static boolean passIt(Iterator<?> it, int count) {
+        for (var i = 0; i < count; i++) {
+            if (!it.hasNext())
+                return true;
+            it.next();
+        }
+        return false;
+    }
+
     @Override
     public void loadTriples(Model model) throws ScraperException {
         try {
             var doc = Jsoup.connect(METEOCIEL_PAGE + getCityId()).get();
-            doc.select("td").forEach(System.out::println);
-            // TODO: complete that
+            var elements = doc
+                    .select("tr td center table tr td");
+            var it = elements.iterator();
+            if (passIt(it, 28))
+                throw new ScraperException(this, "Can't pass header td");
+            for (; it.hasNext();) {
+                var hour = it.next().text();
+
+                if (passIt(it, 3) || !it.hasNext())
+                    throw new ScraperException(this, "Can't pass hour to temperature");
+
+                var temp = it.next().text();
+
+                System.out.println(hour + ": " + temp);
+                // TODO: complete that to add triples
+
+                passIt(it, 7);
+            }
 
         } catch (IOException e) {
             throw new ScraperException(this, e);
