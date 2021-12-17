fce9dc0910bf5b231f28d598e1cd9534a5f59871
ATE47 <wilate@yahoo.fr>
2021-12-17 14:56:59 +0100

    add territoire scraper

--- Scrapper/src/main/java/Start.java
+++ Scrapper/src/main/java/Start.java
@@ -2,10 +2,12 @@ import org.apache.jena.rdf.model.ModelFactory;
 
 import fr.atesab.sw.project.scraper.ScraperException;
 import fr.atesab.sw.project.scraper.meteo.MeteoScraper;
+import fr.atesab.sw.project.territoire.Territoire;
 
 public class Start {
     public static void main(String[] args) {
-        var scraper = new MeteoScraper(MeteoScraper.METEOCIEL_ID_SAINT_ETIENNE);
+        // var scraper = new MeteoScraper(MeteoScraper.METEOCIEL_ID_SAINT_ETIENNE);
+        var scraper = new Territoire(Territoire.TERRITOIRE_URL);
 
         var model = ModelFactory.createDefaultModel();
 
---
+++ Scrapper/src/main/java/fr/atesab/sw/project/territoire/Territoire.java
@@ -0,0 +1,68 @@
+package fr.atesab.sw.project.territoire;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import javax.lang.model.element.Element;
+
+import org.apache.jena.rdf.model.Model;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import fr.atesab.sw.project.scraper.Scraper;
+import fr.atesab.sw.project.scraper.ScraperException;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class Territoire extends Scraper {
+    public static final String TERRITOIRE_URL = "https://territoire.emse.fr/kg/";
+
+    private void allFiles0(String url, List<String> files, Pattern regex) {
+        Document d;
+        try {
+            d = Jsoup.connect(url).get();
+        } catch (Exception e) {
+            throw new ScraperException(this, "can't connect and get to url '" + url + "'", e);
+        }
+        d.select("a[href]").forEach(a -> {
+            String link = a.attr("href");
+            if (link.startsWith("/"))
+                return;
+
+            if (link.endsWith("/")) {
+                allFiles0(a.absUrl("href"), files, regex);
+                return;
+            }
+
+            if (regex.matcher(link).matches()) {
+                files.add(a.absUrl("href"));
+            }
+        });
+    }
+
+    public List<String> allFiles(String pattern) {
+        Pattern regex = Pattern.compile(pattern);
+        List<String> files = new ArrayList<>();
+        allFiles0(this.url, files, regex);
+        return files;
+    }
+
+    @NonNull
+    private final String url;
+
+    @Override
+    public String getName() {
+        return "territoire";
+    }
+
+    @Override
+    public void loadTriples(Model model) throws ScraperException {
+        List<String> ttls = allFiles(".*[.]ttl");
+
+        for (String ttl : ttls)
+            System.out.println(ttl);
+    }
+}
