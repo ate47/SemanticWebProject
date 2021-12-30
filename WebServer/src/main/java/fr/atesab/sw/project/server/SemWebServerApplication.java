f09cdc05969cc1081497d1279f75db3ce6159d1f
ATE47 <wilate@yahoo.fr>
2021-12-30 17:56:57 +0100

    add webserver
    Signed-off-by: ATE47 <wilate@yahoo.fr>

---
+++ WebServer/src/main/java/fr/atesab/sw/project/server/SemWebServerApplication.java
@@ -0,0 +1,13 @@
+package fr.atesab.sw.project.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemWebServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemWebServerApplication.class, args);
+	}
+
+}
---
+++ WebServer/src/test/java/fr/atesab/sw/project/server/SemWebServerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.atesab.sw.project.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemWebServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
