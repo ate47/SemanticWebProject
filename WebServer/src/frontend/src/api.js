40509304547347c36ec635e1ecb1237971318ee7
ATE47 <wilate@yahoo.fr>
2022-01-06 06:19:28 +0100

    fe link floor and rooms

--- WebServer/src/frontend/src/api.js
+++ WebServer/src/frontend/src/api.js
@@ -21,6 +21,28 @@ export default {
       .get(backendEndpoint + "/dataterritoire")
       .then((result) => result.data);
   },
+
+  /**
+   * requests the floors of the main building
+   * @returns {Promise<Array<string>>} the api return value
+   */
+  floors: async function () {
+    return axios
+      .get(backendEndpoint + "/territoire/floors")
+      .then((result) => result.data.floors);
+  },
+
+  /**
+   * requests the rooms of a floor
+   * @param {string} floor the floor
+   * @returns {Promise<Array<string>>} the api return value
+   */
+  rooms: async function (floor) {
+    return axios
+      .get(backendEndpoint + "/territoire/rooms?floor=" + floor)
+      .then((result) => result.data.rooms);
+  },
+
   /**
    * request the scraping of the meteociel
    * @returns {Promise<any>} the api return value
